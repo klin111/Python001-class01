b730a5c089963e87d9dc87e7f7793d419574a034
leo-wang <klin111@qq.com>
2020-07-05 17:01:57 +0800

    add webdriver shimo

---
+++ week02/p5_cookie_webdriver_shimo_com.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+    # 需要安装chrome driver, 和浏览器版本保持一致
+    # http://chromedriver.storage.googleapis.com/index.html
+    
+    browser.get('https://shimo.im')
+    time.sleep(1)
+    # browser.switch_to_frame(browser.find_elements_by_tag_name('iframe')[0])
+    btm1 = browser.find_element_by_xpath('/html/body/div[1]/div[1]/header/nav/div[3]/a[2]/button')
+    btm1.click()
+    time.sleep(5)
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys('15055495@qq.com')
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys('test123test456')
+    time.sleep(3)
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/button').click()
+
+    cookies = browser.get_cookies() # 获取cookies
+    print(cookies)
+    time.sleep(3)
+
+except Exception as e:
+    print(e)
+finally: 
+    time.sleep(10)   
+    browser.close()
+    
