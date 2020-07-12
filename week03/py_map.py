41f4d8cd622f7ea8e72ad07a0ba58b1f30a2f2bc
leo-wang <klin111@qq.com>
2020-07-13 00:03:06 +0800

    add nc scan

--- week03/py_map.py
+++ week03/py_map.py
@@ -3,9 +3,17 @@ from concurrent.futures import ThreadPoolExecutor
 import subprocess
 
 
+def ping(ip):
+    p=subprocess.run(['ping','ip','-t','1'])
+
+def nc():
+    port_range='1-1024'
+    n=subprocess.run(['nc','rz','w','2','ip','port_range' ])
+
 def ping_f(n, f, ip):
     # 把ip地址格式标准化，用map方式加入线程池
     if f == 'ping':
+
         print(111 )
      
     elif f == 'tcp':
